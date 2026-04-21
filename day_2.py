5c4bc3477394ff2467fb0f92a2d3d0bcd6d300e4
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-04-21 19:45:33 +0530

    day 2 of practiting python

---
+++ day_2.py
@@ -0,0 +1,50 @@
+# program 4 to swap two variables
+
+# a = input("Enter value of the first value :  ")
+# b = input("Enter value of the second value :  ")
+
+# print(f"Original values that need to be swapned : a : {a} b : {b}")
+
+# temp = a 
+# a = b 
+# b = temp
+
+# print(f"Swapped values: a : {a}  b : {b}")
+
+# Program 5 to generate a random number
+
+# import random
+
+# print(f"Random number : {random.randint(1, 100)}")
+
+# Program 6 write a program to convert kilometers to miles
+
+# kilometers = float(input("Enter the distance in kilometers : "))
+
+# # Conversion factor : 1 kilometer = 0.621371
+
+# conversion_factor = 0.621371
+
+# miles = kilometers * conversion_factor
+
+# print(f"{kilometers} kilometers is equal to {miles} miles.")
+
+# Program to convert Celsius to Fahrenheit
+
+# celsius = float(input("Enter temperature in Celsius : "))
+
+# # Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+
+# fahrenheit = (celsius * (9/5)) + 32
+
+# print(f"{celsius} degrees Celsius is equal to {fahrenheit} degree Fahrenheit")
+
+# Write a program to to display calendar1
+
+# import calendar
+
+# year = int(input("Enter year : "))
+# month = int(input("Enter month : "))
+
+# cal = calendar.month(year,month)
+# print(cal)
